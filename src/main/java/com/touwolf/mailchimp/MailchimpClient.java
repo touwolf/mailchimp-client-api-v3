49cb1af36e7d73c16c2e09d43f86c64c01dc0588
gorums <acksecurity@hotmail.com>
2016-05-30 10:55:42 -0500

    Add the mailchimp client class

---
+++ src/main/java/com/touwolf/mailchimp/MailchimpClient.java
@@ -0,0 +1,11 @@
+package com.touwolf.mailchimp;
+
+public class MailchimpClient
+{
+    private String apiKey;
+
+    public MailchimpClient(String apiKey)
+    {
+        this.apiKey = apiKey;
+    }
+}

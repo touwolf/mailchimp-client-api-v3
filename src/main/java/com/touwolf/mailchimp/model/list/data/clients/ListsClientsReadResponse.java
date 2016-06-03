de823c04909e076b5688ef70d99ecb1176e2018a
gorums <acksecurity@hotmail.com>
2016-06-03 17:52:13 -0500

    Working on ListsClients methods

---
+++ src/main/java/com/touwolf/mailchimp/model/list/data/clients/ListsClientsReadResponse.java
@@ -0,0 +1,68 @@
+package com.touwolf.mailchimp.model.list.data.clients;
+
+import com.google.gson.annotations.SerializedName;
+import com.touwolf.mailchimp.model.global.Links;
+
+import java.util.List;
+
+public class ListsClientsReadResponse
+{
+    private List<ListsClientsResponse> clients;
+
+    @SerializedName("list_id")
+    private String listId;
+
+    @SerializedName("total_items")
+    private Integer totalItems;
+
+    @SerializedName("_links")
+    private List<Links> links;
+
+    /**
+     * An array of top email clients.
+     *
+     */
+    public List<ListsClientsResponse> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<ListsClientsResponse> clients) {
+        this.clients = clients;
+    }
+
+    /**
+     * The list id.
+     *
+     */
+    public String getListId() {
+        return listId;
+    }
+
+    public void setListId(String listId) {
+        this.listId = listId;
+    }
+
+    /**
+     * The total number of items matching the query regardless of pagination.
+     *
+     */
+    public Integer getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(Integer totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    /**
+     * A list of link types and descriptions for the API schema documents.
+     *
+     */
+    public List<Links> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Links> links) {
+        this.links = links;
+    }
+}
---
+++ src/main/java/com/touwolf/mailchimp/model/list/data/clients/ListsClientsResponse.java
@@ -0,0 +1,67 @@
+package com.touwolf.mailchimp.model.list.data.clients;
+
+import com.google.gson.annotations.SerializedName;
+import com.touwolf.mailchimp.model.global.Links;
+
+import java.util.List;
+
+public class ListsClientsResponse
+{
+    private String client;
+
+    private Integer members;
+
+    @SerializedName("list_id")
+    private String listId;
+
+    @SerializedName("_links")
+    private List<Links> links;
+
+    /**
+     * The name of the email client.
+     *
+     */
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    /**
+     * The number of subscribed members who used this email client.
+     *
+     */
+    public Integer getMembers() {
+        return members;
+    }
+
+    public void setMembers(Integer members) {
+        this.members = members;
+    }
+
+    /**
+     * The unique id for the list.
+     *
+     */
+    public String getListId() {
+        return listId;
+    }
+
+    public void setListId(String listId) {
+        this.listId = listId;
+    }
+
+    /**
+     * A list of link types and descriptions for the API schema documents.
+     *
+     */
+    public List<Links> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Links> links) {
+        this.links = links;
+    }
+}

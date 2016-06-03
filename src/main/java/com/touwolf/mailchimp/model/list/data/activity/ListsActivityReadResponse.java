25afbea7101f738c2d8efae6629a8e520b400747
gorums <acksecurity@hotmail.com>
2016-06-03 09:59:01 -0500

    Working on ListsActivity methods

---
+++ src/main/java/com/touwolf/mailchimp/model/list/data/activity/ListsActivityReadResponse.java
@@ -0,0 +1,68 @@
+package com.touwolf.mailchimp.model.list.data.activity;
+
+import com.google.gson.annotations.SerializedName;
+import com.touwolf.mailchimp.model.global.Links;
+
+import java.util.List;
+
+public class ListsActivityReadResponse
+{
+    private List<ListsActivityRespose> activity;
+
+    @SerializedName("list_id")
+    private String listId;
+
+    @SerializedName("total_items")
+    private Integer totalItems;
+
+    @SerializedName("_links")
+    private List<Links> links;
+
+    /**
+     * Recent list activity.
+     *
+     */
+    public List<ListsActivityRespose> getActivity() {
+        return activity;
+    }
+
+    public void setActivity(List<ListsActivityRespose> activity) {
+        this.activity = activity;
+    }
+
+    /**
+     * The unique id for the list.
+     *
+     */
+    public String getListId() {
+        return listId;
+    }
+
+    public void setListId(String listId) {
+        this.listId = listId;
+    }
+
+    /**
+     * The total number of items matching the query regardless of pagination.
+     *
+     */
+    public Integer getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(Integer totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    /**
+     * A list of link types and descriptions for the API schema documents.
+     *
+     */
+    public List<Links> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Links> links) {
+        this.links = links;
+    }
+}
---
+++ src/main/java/com/touwolf/mailchimp/model/list/data/activity/ListsActivityRespose.java
@@ -0,0 +1,171 @@
+package com.touwolf.mailchimp.model.list.data.activity;
+
+import com.google.gson.annotations.SerializedName;
+import com.touwolf.mailchimp.model.global.Links;
+
+import java.util.List;
+
+public class ListsActivityRespose
+{
+    private String day;
+
+    @SerializedName("emails_sent")
+    private Integer emailsSent;
+
+    @SerializedName("unique_opens")
+    private Integer uniqueOpens;
+
+    @SerializedName("recipient_clicks")
+    private Integer recipientClicks;
+
+    @SerializedName("hard_bounce")
+    private Integer hardBounce;
+
+    @SerializedName("soft_bounce")
+    private Integer softBounce;
+
+    private Integer subs;
+
+    private Integer unsubs;
+
+    @SerializedName("other_adds")
+    private Integer otherAdds;
+
+    @SerializedName("other_removes")
+    private Integer otherRemoves;
+
+    @SerializedName("_links")
+    private List<Links> links;
+
+    /**
+     * The date for the activity summary.
+     *
+     */
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    /**
+     * The total number of emails sent on the date for the activity summary.
+     *
+     */
+    public Integer getEmailsSent() {
+        return emailsSent;
+    }
+
+    public void setEmailsSent(Integer emailsSent) {
+        this.emailsSent = emailsSent;
+    }
+
+    /**
+     * The number of unique opens.
+     *
+     */
+    public Integer getUniqueOpens() {
+        return uniqueOpens;
+    }
+
+    public void setUniqueOpens(Integer uniqueOpens) {
+        this.uniqueOpens = uniqueOpens;
+    }
+
+    /**
+     * The number of clicks.
+     *
+     */
+    public Integer getRecipientClicks() {
+        return recipientClicks;
+    }
+
+    public void setRecipientClicks(Integer recipientClicks) {
+        this.recipientClicks = recipientClicks;
+    }
+
+    /**
+     * The number of hard bounces.
+     *
+     */
+    public Integer getHardBounce() {
+        return hardBounce;
+    }
+
+    public void setHardBounce(Integer hardBounce) {
+        this.hardBounce = hardBounce;
+    }
+
+    /**
+     * The number of soft bounces
+     *
+     */
+    public Integer getSoftBounce() {
+        return softBounce;
+    }
+
+    public void setSoftBounce(Integer softBounce) {
+        this.softBounce = softBounce;
+    }
+
+    /**
+     * The number of subscribes.
+     *
+     */
+    public Integer getSubs() {
+        return subs;
+    }
+
+    public void setSubs(Integer subs) {
+        this.subs = subs;
+    }
+
+    /**
+     * The number of unsubscribes.
+     *
+     */
+    public Integer getUnsubs() {
+        return unsubs;
+    }
+
+    public void setUnsubs(Integer unsubs) {
+        this.unsubs = unsubs;
+    }
+
+    /**
+     * The number of subscribers who may have been added outside of the double opt-in process, such as imports or API activity.
+     *
+     */
+    public Integer getOtherAdds() {
+        return otherAdds;
+    }
+
+    public void setOtherAdds(Integer otherAdds) {
+        this.otherAdds = otherAdds;
+    }
+
+    /**
+     * The number of subscribers who may have been removed outside of unsubscribing or reporting an email as spam (for example, deleted subscribers).
+     *
+     */
+    public Integer getOtherRemoves() {
+        return otherRemoves;
+    }
+
+    public void setOtherRemoves(Integer otherRemoves) {
+        this.otherRemoves = otherRemoves;
+    }
+
+    /**
+     * A list of link types and descriptions for the API schema documents.
+     *
+     */
+    public List<Links> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Links> links) {
+        this.links = links;
+    }
+}

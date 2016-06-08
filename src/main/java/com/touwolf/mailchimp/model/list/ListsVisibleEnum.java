643d18ff15a58239c9244e727fe01a96cd756111
gorums <acksecurity@hotmail.com>
2016-06-08 16:23:44 -0500

    Add enum into the lists API

---
+++ src/main/java/com/touwolf/mailchimp/model/list/ListsVisibleEnum.java
@@ -0,0 +1,11 @@
+package com.touwolf.mailchimp.model.list;
+
+import com.google.gson.annotations.SerializedName;
+
+public enum ListsVisibleEnum
+{
+    @SerializedName("pub")
+    PUB,
+    @SerializedName("prv")
+    PRV
+}
---
+++ src/main/java/com/touwolf/mailchimp/model/list/interestcategories/ListsInterestCategoriesTypeEnum.java
@@ -0,0 +1,15 @@
+package com.touwolf.mailchimp.model.list.interestcategories;
+
+import com.google.gson.annotations.SerializedName;
+
+public enum ListsInterestCategoriesTypeEnum
+{
+    @SerializedName("checkboxes")
+    CHECK_BOXES,
+    @SerializedName("dropdown")
+    DROP_DOWN,
+    @SerializedName("radio")
+    RADIO,
+    @SerializedName("hidden")
+    HIDDEN
+}

d9f4df838c5a41f21d2693aea49a80ba5a370870
gorums <acksecurity@hotmail.com>
2016-06-09 10:15:04 -0500

    Add test files

---
+++ src/test/java/com/touwolf/mailchimp/MailchimpListsInterestCategoriesTest.java
@@ -0,0 +1,49 @@
+package com.touwolf.mailchimp;
+
+import com.touwolf.mailchimp.api.campaign.Campaign;
+import com.touwolf.mailchimp.api.lists.ListsInterestCategories;
+import com.touwolf.mailchimp.data.MailchimpResponse;
+import com.touwolf.mailchimp.impl.MailchimpClientImpl;
+import com.touwolf.mailchimp.model.campaign.CampaignReadRequest;
+import com.touwolf.mailchimp.model.campaign.CampaignReadResponse;
+import org.junit.*;
+
+public class MailchimpListsInterestCategoriesTest {
+    // To execute this test correctly, please fill real username and apikey
+    private static final String USERNAME = "";
+
+    private static final String API_KEY = "";
+
+    public MailchimpListsInterestCategoriesTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSendgridRequest() throws Exception {
+
+        try {
+            MailchimpClient client = new MailchimpClientImpl(USERNAME, API_KEY);
+            ListsInterestCategories listsInterestCategories = client.listsInterestCategories();
+
+            //Assert.assertNotNull(response);
+        } catch (Exception ex) {
+            Assert.assertTrue(false);
+        }
+    }
+
+}
